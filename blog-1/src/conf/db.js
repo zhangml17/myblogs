4c7df26e946cc6a4354d429df5eb9b5f41da1bbe
zhangmeilong <zhangml@xdgmt.com>
2019-08-15 15:34:27 +0800

    update: 封装redis

--- blog-1/src/conf/db.js
+++ blog-1/src/conf/db.js
@@ -1,5 +1,6 @@
 const env = process.env.NODE_ENV;
 let MYSQL_CONF
+let REDIS_CONF
 
 // 开发环境
 if(env === 'development'){
@@ -9,6 +10,14 @@ if(env === 'development'){
         password:'root',
         port:'3333',
         database:'aaaaa'
+    }
+
+    REDIS_CONF = {
+        port:6379,
+        host:'192.168.100.200',
+        opts: {
+            auth_pass:'root'
+        }
     }
 }
 
@@ -21,7 +30,16 @@ if(env === 'production'){
         port:'3333',
         database:'aaaaa'
     }
+
+    REDIS_CONF = {
+        port:6379,
+        host:'192.168.100.200',
+        opts: {
+            auth_pass:'root'
+        }
+    }
 }
 module.exports = {
-    MYSQL_CONF
+    MYSQL_CONF,
+    REDIS_CONF
 }
---
+++ blog-1/src/db/redis.js
@@ -0,0 +1,47 @@
+const { REDIS_CONF } = require('../conf/db')
+const redis = require('redis')
+
+const redisCli = redis.createClient(REDIS_CONF.port,REDIS_CONF.host,REDIS_CONF.opts)
+
+redisCli.on('error',err=>{
+    console.error(err)
+})
+
+
+// 封装存、取操作
+function set(key,val){
+    if(typeof val === 'object'){
+        val = JSON.stringify(val)
+    }
+    redisCli.set(ley,val,redis.print)
+}
+
+function get(key){
+    const promise = new Promise((resolve,reject)=>{
+        redisCli.get(key,(err,val)=>{
+            if(err){
+                reject(err)
+                return
+            }
+
+            if(val == null){
+                resolve(null)
+                return
+            }
+
+            try{
+                resolve(JSON.parse(val))
+            }catch(ex){
+                resolve(val)
+            }
+            
+            redisCli.quit()
+        })
+    })
+    return promise
+}
+
+module.exports = {
+    get,
+    set
+}
